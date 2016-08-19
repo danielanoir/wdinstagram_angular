7c5f988a31193ad9fc978745eb7b35f28a9ed482
Daniela Noir <daniela@danielanoir.com>
2016-08-18 20:27:07 -0400

    hw

---
+++ js/todos.controller.js
@@ -0,0 +1,9 @@
+var app = angular.module("argh")
+app.controller("arghController", function(){
+  this.argh = [
+    "Hi I'm angry",
+    "Want to go for a bikeride?",
+    "Nevermind, it's too hot.",
+    "Argh, still so angry help."
+  ]
+});
